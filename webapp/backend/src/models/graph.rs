8115a4c5046ecbd99038a18d9aad4653917e81a3
ryusukeyasiro <ryusukeyasiro@gmail.com>
2024-07-27 20:28:04 +0900

    seach

--- webapp/backend/src/models/graph.rs
+++ webapp/backend/src/models/graph.rs
@@ -1,5 +1,6 @@
+// models/graph.rs
 use sqlx::FromRow;
-use std::collections::HashMap;
+use std::collections::BinaryHeap;
 
 #[derive(FromRow, Clone, Debug)]
 pub struct Node {
@@ -17,60 +18,63 @@ pub struct Edge {
 
 #[derive(Debug)]
 pub struct Graph {
-    pub nodes: HashMap<i32, Node>,
-    pub edges: HashMap<i32, Vec<Edge>>,
+    pub nodes: Vec<Node>,
+    pub edges: Vec<Vec<Edge>>,
 }
 
 impl Graph {
     pub fn new() -> Self {
         Graph {
-            nodes: HashMap::new(),
-            edges: HashMap::new(),
+            nodes: vec![],
+            edges: vec![],
         }
     }
 
     pub fn add_node(&mut self, node: Node) {
-        self.nodes.insert(node.id, node);
+        while self.nodes.len() <= node.id as usize {
+            self.nodes.push(Node { id: self.nodes.len() as i32, x: 0, y: 0 });
+            self.edges.push(vec![]);
+        }
+        self.nodes[node.id as usize] = node.clone();
     }
 
     pub fn add_edge(&mut self, edge: Edge) {
-        self.edges
-            .entry(edge.node_a_id)
-            .or_default()
-            .push(edge.clone());
-
+        self.edges[edge.node_a_id as usize].push(edge.clone());
         let reverse_edge = Edge {
             node_a_id: edge.node_b_id,
             node_b_id: edge.node_a_id,
             weight: edge.weight,
         };
-        self.edges
-            .entry(reverse_edge.node_a_id)
-            .or_default()
-            .push(reverse_edge);
+        self.edges[reverse_edge.node_a_id as usize].push(reverse_edge);
     }
 
-    pub fn shortest_path(&self, from_node_id: i32, to_node_id: i32) -> i32 {
-        let mut distances = HashMap::new();
-        distances.insert(from_node_id, 0);
+    pub fn dijkstra(&self, start_id: i32, goal_id: i32) -> i32 {
+        let mut distances = vec![i32::MAX; self.nodes.len()];
+        let mut heap = BinaryHeap::new();
 
-        for _ in 0..self.nodes.len() {
-            for node_id in self.nodes.keys() {
-                if let Some(edges) = self.edges.get(node_id) {
-                    for edge in edges {
-                        let new_distance = distances
-                            .get(node_id)
-                            .and_then(|d: &i32| d.checked_add(edge.weight))
-                            .unwrap_or(i32::MAX);
-                        let current_distance = distances.get(&edge.node_b_id).unwrap_or(&i32::MAX);
-                        if new_distance < *current_distance {
-                            distances.insert(edge.node_b_id, new_distance);
-                        }
-                    }
+        distances[start_id as usize] = 0;
+        heap.push((std::cmp::Reverse(0), start_id));
+
+        while let Some((std::cmp::Reverse(current_distance), current_node)) = heap.pop() {
+            if current_node == goal_id {
+                return current_distance;
+            }
+
+            if current_distance > distances[current_node as usize] {
+                continue;
+            }
+
+            for edge in &self.edges[current_node as usize] {
+                let next = edge.node_b_id;
+                let next_distance = current_distance + edge.weight;
+
+                if next_distance < distances[next as usize] {
+                    distances[next as usize] = next_distance;
+                    heap.push((std::cmp::Reverse(next_distance), next));
                 }
             }
         }
 
-        distances.get(&to_node_id).cloned().unwrap_or(i32::MAX)
+        distances[goal_id as usize]
     }
 }
